e4b5901ea8d96c457d24eaf8b0aad1280dfed093
Lavina-23 <lavinakohai3227@gmail.com>
2025-02-18 14:22:40 +0700

    feat: controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -33,3 +39,27 @@ Route::get('/articles/{id}', function ($id) {
 Route::get('/user/{name?}', function ($name = null) {
     return 'Nama saya ' . $name;
 });
+
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)
+    ->only([
+        'index',
+        'show'
+    ]);
+
+Route::resource('photos', PhotoController::class)
+    ->except([
+        'create',
+        'store',
+        'update',
+        'destroy'
+    ]);
